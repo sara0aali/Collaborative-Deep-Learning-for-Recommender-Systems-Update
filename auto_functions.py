c91cc59398a5e3a040b86b0b728671d7569316d3
sara0aali <124429504+sara0aali@users.noreply.github.com>
2025-01-08 16:24:51 +0330

    Update auto_functions.py

--- auto_functions.py
+++ auto_functions.py
@@ -83,7 +83,13 @@ def autoEncoder(ratio_l, ratio_u, batch, W1, W2, xtrain, x_new, u, b1, b2, c1, c
         # به‌روزرسانی وزن‌ها (اینجا باید کد واقعی قرار گیرد)
 
     # محاسبات خروجی اتواینکودر
-    hidden = getoutPut(W1, W2, b1, b2, xtrain, accList)
+  def getoutPut(W1, W2, b1, b2, xtrain, accList):
+    # محاسبات اتواینکودر
+    hidden = np.dot(xtrain, W1) + b1
+    hidden = np.maximum(hidden, 0)  # اعمال ReLU
+    hidden = np.dot(hidden, W2) + b2
+    return hidden
+
 
     # ذخیره ماتریس‌های نهایی
     with h5py.File('u_final_auto.h5', 'w') as hf:
@@ -103,7 +109,5 @@ def autoEncoder(ratio_l, ratio_u, batch, W1, W2, xtrain, x_new, u, b1, b2, c1, c
     with h5py.File('c2_final.h5', 'w') as hf:
         hf.create_dataset("c2", data=c2)
 
-    # بازگشت خروجی
-    return hidden
 
 main(denoise=True)
